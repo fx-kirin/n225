ef64ac794a9229a07d282f27b4b31bf6d65ffd76
fx_kirin <fx.kirin@gmail.com>
2021-01-07 17:02:41 +0900

    fix bug

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_requires():
 
 setup(
     name="n225",
-    version="0.1.16",
+    version="0.1.17",
     url="https://github.com/fx-kirin/n225",
     license="MIT",
     author="fx-kirin",
