342242de7349460b59356d523679bffd7dde5158
fx_kirin <fx.kirin@gmail.com>
2024-10-04 08:40:46 +0900

    fix typo

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_requires():
 
 setup(
     name="n225",
-    version="0.1.29",
+    version="0.1.30",
     url="https://github.com/fx-kirin/n225",
     license="MIT",
     author="fx-kirin",
